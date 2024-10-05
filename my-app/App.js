5ff0319d12f86d02a37df0ef18ba249c3d2cafed
Hon139 <andrew.yang139@gmail.com>
2024-10-05 12:53:03 -0400

    package install

---
+++ my-app/App.js
@@ -0,0 +1,77 @@
+import React from "react";
+import {
+  Text,
+  Link,
+  HStack,
+  Center,
+  Heading,
+  Switch,
+  useColorMode,
+  NativeBaseProvider,
+  extendTheme,
+  VStack,
+  Box,
+  Button,
+} from "native-base";
+import NativeBaseIcon from "./components/NativeBaseIcon";
+import { Platform } from "react-native";
+
+// Define the config
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: "dark",
+};
+
+// extend the theme
+export const theme = extendTheme({ config });
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <Center
+        _dark={{ bg: "blueGray.900" }}
+        _light={{ bg: "blueGray.50" }}
+        px={4}
+        flex={1}
+      >
+        <VStack space={5} alignItems="center">
+          <Heading size="lg" py={5}>Cart Sheriff</Heading>
+
+
+          <NativeBaseIcon />
+          {/* <HStack space={2} alignItems="center">
+            
+          </HStack> */}
+          {/* <Link href="https://docs.nativebase.io" isExternal>
+            <Text color="primary.500" underline fontSize={"xl"}>
+              Learn NativeBase
+            </Text>
+          </Link> */}
+          <ToggleDarkMode />
+
+
+          <Button onPress={() => console.log("hello world")}>Logout</Button>
+
+        </VStack>
+      </Center>
+    </NativeBaseProvider>
+  );
+}
+
+// Color Switch Component
+function ToggleDarkMode() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <HStack space={2} alignItems="center">
+      <Text>Dark</Text>
+      <Switch
+        isChecked={colorMode === "light"}
+        onToggle={toggleColorMode}
+        aria-label={
+          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
+        }
+      />
+      <Text>Light</Text>
+    </HStack>
+  );
+}
